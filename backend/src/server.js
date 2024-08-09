c6bb4917dce92a34511f95c51f491ff34ce5910d
jakaria242 <jakaria.dev242@gmail.com>
2024-08-09 22:33:03 +0600

    project setup

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const  dotenv = require('dotenv')
+dotenv.config()
+
+
+
+const port = process.env.PORT || 8000 ;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is runing on port ${port}`)
+})
